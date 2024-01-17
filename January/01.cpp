2a44b2b490de4d4c01746a668ac94bd845216af5
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-01-17 15:02:32 +0000

    First problem - commit

--- January/01.cpp
+++ January/01.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void bit(){
+// Codeforces - Bit++ Problem
+  int n;
+  cin >> n;
+  vector<string> vect;
+  for(int i=0;i<n;i++){
+    string temp;
+    cin>>temp;
+    vect.push_back(temp);
+  }
+
+  int x=0;
+  for(int i=0;i<n;i++){
+    if(vect[i][1]=='+'){
+      x+=1;
+    }
+    else{
+      x-=1;
+    }
+  }
+  cout<<x<<endl;
+}
+
+int main(){
+    bit();
+    return 0;
+}
