9711a0525b474d26b1757ee9d5802ea5543d7676
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-01-23 16:48:47 +0000

    Problem 4 done

--- January/03.cpp
+++ January/03.cpp
@@ -106,6 +106,121 @@ bool isValidParenthesis(string s)
     return true;
 }
 
+class Deque
+{
+public:
+    // Initialize your data structure.
+    vector<int> vect;
+    int size;
+    Deque(int n)
+    {
+        // Write your code here.
+        size=n;
+    }
+
+    // Pushes 'X' in the front of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushFront(int x)
+    {
+        if(isFull()){
+            return false;
+        }
+        
+        if(!isEmpty()){
+            vect.push_back(vect[vect.size()-1]);
+        
+            for(int i=vect.size()-2;i>=0;i--){
+                vect[i+1]=vect[i];
+            }
+            vect[0]=x;
+        }
+        else{
+            vect.push_back(x);
+        }        
+        return true;
+    }
+
+    // Pushes 'X' in the back of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushRear(int x)
+    {
+        if(isFull()){
+            return false;
+        }
+        // Write your code here.
+        vect.push_back(x);
+        return true;
+    }
+
+    // Pops an element from the front of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
+    int popFront()
+    {
+        // Write your code here.
+        if(isEmpty()){
+            return -1;
+        }
+        int temp=getFront();
+
+        for(int i=0;i<vect.size()-1;i++){
+            vect[i]=vect[i+1];
+        }
+        vect.pop_back();
+        return temp;
+    }
+
+    // Pops an element from the back of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
+    int popRear()
+    {
+
+        if(isEmpty()){
+            return -1;
+        }
+        // Write your code here.
+        int temp=getRear();
+        vect.pop_back();
+        return temp;
+
+    }
+
+    // Returns the first element of the deque. If the deque is empty, it returns -1.
+    int getFront()
+    {
+        if(isEmpty()){
+            return -1;
+        }
+        // Write your code here.
+        return vect[0];
+    }
+
+    // Returns the last element of the deque. If the deque is empty, it returns -1.
+    int getRear()
+    {
+        if(isEmpty()){
+            return -1;
+        }
+        // Write your code here.
+        return vect[vect.size()-1];
+    }
+
+    // Returns true if the deque is empty. Otherwise returns false.
+    bool isEmpty()
+    {
+        if(vect.size()==0){
+            return true;
+        }
+        return false;
+        // Write your code here.
+    }
+
+    // Returns true if the deque is full. Otherwise returns false.
+    bool isFull()
+    {
+        if(size == vect.size()){
+            return true;
+        }
+        return false;
+        // Write your code here.
+    }
+};
+
 int main(){
 
     return 0;
