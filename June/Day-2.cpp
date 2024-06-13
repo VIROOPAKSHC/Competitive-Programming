491e5a0d3134de3f9e52a2999ea6f7c5bd552486
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-13 15:46:39 +0000

    Problem-6 done

--- June/Day-2.cpp
+++ June/Day-2.cpp
@@ -49,6 +49,7 @@ int Sum1toN(int N){
 }
 
 void RecursiveReverse(vector<int>& vect){
+    // Problem-5
     if(vect.size()==1){
         return;
     }
@@ -59,9 +60,81 @@ void RecursiveReverse(vector<int>& vect){
     vect.insert(vect.begin(),temp);
 }
 
+class Solution {
+public:
+    bool isPalindrome_Imperfect(string s) {
+        // Fails extremely large test case - memory limit exceeded.
+        if(s.size()<=1){
+            return true;
+        }
+        
+        int n = s.size();
+        int i=0,j=n-1;
+        if(((s[i]<='z') && (s[i]>='a'))||((s[i]>='A') && (s[i]<='Z')) || (s[i]>='0' && s[i]<='9') ){
+            if(s[i]<='Z'){
+                s[i] = s[i]+32;
+            }
+        }
+        else{
+            while(i<n-1){
+                i++;
+                if(((s[i]<='z') && (s[i]>='a'))||((s[i]>='A') && (s[i]<='Z'))|| (s[i]>='0' && s[i]<='9') ){
+                    if(s[i]<='Z'){
+                        s[i] = s[i]+32;
+                        
+                    }
+                    break;
+                 }
+            }
+        }
+        if(((s[j]<='z') && (s[j]>='a'))||((s[j]>='A') && (s[j]<='Z'))|| (s[j]>='0' && s[j]<='9') ){
+            if(s[j]<='Z'){
+                s[j] = s[j]+32;
+            }
+        }
+        else{
+             while(j>0){
+                j--;
+                if(((s[j]<='z') && (s[j]>='a'))||((s[j]>='A') && (s[j]<='Z'))|| (s[j]>='0' && s[j]<='9') ){
+                    if(s[j]<='Z'){
+                        s[j] = s[j]+32;
+                    }
+                    break;
+                 }
+            }
+        }
+        
+        if((i>j) || ((i==n) || (j==n))){
+            return true;
+        }
+        
+        return (s[i]==s[j]) && isPalindrome_Imperfect(s.substr(i+1,j-i-1));
+    }
+    bool isPalindrome_Perfect(string s) {
+        vector<char> vect;
+        for(int i=0;i<s.size();i++){
+            if((s[i]>=97 && s[i]<=122) || (s[i]>=65 && s[i]<=90) || (s[i]>='0' && s[i]<='9')){
+                vect.push_back(s[i]);
+            }
+        }
+        for(int i=0;i<vect.size();i++){
+            char a=vect[i];
+            char b=vect[vect.size()-1-i];
+            if(a<97)
+                a+=32;
+            if(b<97)
+                b+=32;
+            if(a!=b){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
 int main(){
 
-    vector<int> vect = {1,2,3,4,5,6};
+    vector<int> vect = {-3,43,7,-2};
     RecursiveReverse(vect);
     for(auto a:vect){
         cout<<a<<" ";
