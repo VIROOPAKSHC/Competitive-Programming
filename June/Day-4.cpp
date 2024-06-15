03bb1f936299d106e49cb49bfe91add97f4f77da
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-15 16:55:50 +0000

    problem -2 done

--- June/Day-4.cpp
+++ June/Day-4.cpp
@@ -27,6 +27,33 @@ public:
     }
 };
 
+int FindElement(vector<int> vect,int k){
+    int n = vect.size();
+    int hi=n-1;
+    int lo=0;
+    int mid=(hi+lo)/2;
+    if((k>vect[hi])||(k<vect[lo])){
+        return -1;
+    }
+    while((mid>=0) && (mid<=n-1)){
+        if(vect[mid]==k){
+            return 1;
+        }
+        else if(vect[mid]>k){
+            hi = mid-1;
+            mid = (lo+hi)/2;
+        }
+        else if(vect[mid]<k){
+            lo = mid+1;
+            mid = (lo+hi)/2;
+        }
+        if(lo>mid){
+            return -1;
+        }
+    }
+    return -1;
+}
 int main(){
+    cout<<FindElement({1,2,3,5,6,7},4)<<endl;
     return 0;
 }
