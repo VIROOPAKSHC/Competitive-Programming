a09b3ee5ef9184819ddd985bd90c13e14123c513
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-18 17:24:55 +0000

    Problem-7 done

--- June/Day-7.cpp
+++ June/Day-7.cpp
@@ -112,6 +112,45 @@ int maxProfit(vector<int>& prices) {
        return sell;
     }
 
+void nextPermutation(vector<int>& nums) {
+        int pos=-1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                pos=i;
+                break;
+            }
+        }
+        
+        if(pos==-1){
+            reverse(nums,0);
+        }
+        else{
+            int pos2=-1;
+            for(int i=nums.size()-1; i>=0; i--){
+                if(nums[i]>nums[pos]){
+                    pos2=i;
+                    break;
+                }
+            }
+            swap(nums,pos,pos2);
+            reverse(nums,pos+1);
+        }
+    }
+    void swap(vector<int>& nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    void reverse(vector<int>& nums,int start){
+        int i=start;
+        int j=nums.size()-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+
 int main(){
 
 }
