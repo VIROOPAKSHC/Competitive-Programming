23dda37c4a51f242d78434aa9b93e53a51a9c1eb
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-18 17:12:07 +0000

    Problem-6 done

--- June/Day-7.cpp
+++ June/Day-7.cpp
@@ -103,6 +103,15 @@ vector<int> rearrangeArray(vector<int>& nums) {
         return nums;
 }
 
+int maxProfit(vector<int>& prices) {
+        int buy=INT_MAX,sell=0;
+        for(int i=0;i<prices.size();i++){
+            buy=min(buy,prices[i]);
+            sell=max(sell,prices[i]-buy);
+        }
+       return sell;
+    }
+
 int main(){
 
 }
