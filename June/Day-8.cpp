8e0f2b8c5087548d1a8865f552c268e9291cac0c
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-19 11:27:38 +0000

    Problem-2 done

--- June/Day-8.cpp
+++ June/Day-8.cpp
@@ -18,6 +18,52 @@ vector<int> findLeaders(vector<int> vect){
     return ans;
 }
 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==1){
+            return 1;
+        }
+        
+        set<int> s;
+        for(int i:nums){
+            s.insert({i});
+        }
+        int prev=-1;
+        int c=0,max_count=0;
+        int prev_op=-1;
+        int first_iter=1;
+        for(int i:s){
+            if(prev==-1 && first_iter==1){
+                prev=i;
+                c=1;
+                prev_op=-1;
+                first_iter=0;
+                continue;
+            }
+            if(((i==prev+1) && (prev_op==1 || prev_op==-1))){
+                prev=i;
+                prev_op=1;
+                c++;
+            }
+            else if((i==prev-1) && (prev_op==0 || prev_op==-1)){
+                prev=i;
+                prev_op=0;
+                c++;
+            }
+            else{
+                prev=i;
+                max_count=max(max_count,c);
+                prev_op=-1;
+                c=1;
+            }
+            
+        }
+        max_count=max(max_count,c);
+        return max_count;
+    }
+};
+
 int main(){
     vector<int> a1 = findLeaders({4,7,1,0});
     vector<int> a2=findLeaders({10,22,12,3,0,6});
