0e13994dca27eed19b88d41d14cd6056e2d6cf6b
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-28 14:14:59 +0000

    Problem-5 done

--- June/Day-15.cpp
+++ June/Day-15.cpp
@@ -87,6 +87,36 @@ class Solution {
         }
         return Output;
     }
+    int divide(int dividend, int divisor) {
+        if(dividend == divisor)
+            return 1;
+        int sign=1;
+        if((dividend<0 && divisor>0) || (dividend>0 && divisor<0)){
+            sign = -1;
+        }
+        unsigned int ans=0;
+        unsigned int n=abs(dividend),d=abs(divisor);
+        while(n>=d){
+            int count=0;
+            while(n>(d<<(count+1)))
+                count++;
+            n -= d<<count;
+            ans += 1<<count;
+        }
+        if(ans == (1<<31) && sign==1)
+            return INT_MAX;
+        
+        return sign*ans;
+    }
+    int minBitFlips(int start, int goal) {
+        int c=0;
+        while(start || goal){
+            c+=((start%2)!=(goal%2));
+            start/=2;
+            goal/=2;
+        }
+        return c;
+    }
 };
 
 int main(){
