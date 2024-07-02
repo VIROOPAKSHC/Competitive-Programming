acc6307e326d8c6d7ee158181a20d52d4a4faaf6
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-02 10:16:01 +0000

    Problem-2 done

--- June/Day-19.cpp
+++ June/Day-19.cpp
@@ -20,6 +20,27 @@ public:
         }
         return -1;
     }
+    bool isGood(vector<int>& nums) {
+        if(nums.size()==1){return false;}
+        
+        sort(nums.begin(),nums.end());
+        int n = nums[nums.size()-1];
+        if(n+1 != nums.size()){
+            return false;
+        }
+        if(nums[0]!=1){
+            return false;
+        }
+        for(int i=1;i<nums.size()-1;i++){
+            if(nums[i]==nums[i-1]){
+                return false;
+            }
+        }
+        if(nums[nums.size()-1]!=nums[nums.size()-2]){
+            return false;
+        }
+        return true;
+    }
 };
 
 int main(){
