e428637cb309fac07ae54b219588b8a5a294fe2f
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-03 11:51:09 +0000

    Problem-1 done

--- July/Day-3.cpp
+++ July/Day-3.cpp
@@ -1,3 +1,40 @@
 // Author - Chekuri Viroopaksh
 // Day 20 - 3rd, July, 2024
 // Sorting Leetcode problems
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int c=0;
+        sort(nums.begin(),nums.end());
+        while(true){     
+            if(nums[nums.size()-1]==0){
+                break;
+            }
+            int iter_Val = nums[0];       
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]==0){
+                    continue;
+                }
+                else if(nums[i]!=0 && iter_Val == nums[0]){
+                    iter_Val = nums[i];
+                    nums[i]=0;
+                }
+                else{
+                    nums[i]-=iter_Val;
+                }
+            }
+            c++;
+            
+
+        }
+        return c;
+    }
+};
+
+int main(){
+    return 0;
+}
