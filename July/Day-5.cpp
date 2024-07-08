712ae2c448ed14109803731a26f8be7c0365e4c3
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-08 15:39:26 +0000

    Problem-1 done

---
+++ July/Day-5.cpp
@@ -0,0 +1,29 @@
+// Author - Chekuri Viroopaksh
+// Day 22 - July 8, 2024
+// Sorting Leetcode problems
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int c=0;
+        int start = nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]-start > k){
+                c++;
+                start = nums[i];
+            }
+        }
+        return c+1;
+    }
+    
+};
+
+int main(){
+    vector<int> s;
+    
+    return 0;
+}
