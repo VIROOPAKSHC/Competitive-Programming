9696da2800cdfede5caedfc273f37395ba84535d
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-09 14:52:23 +0000

    Problem-6 done

--- July/Day-6.cpp
+++ July/Day-6.cpp
@@ -133,6 +133,16 @@ public:
         }
         return ans;
     }
+    int minimumPushes(const string &word) {
+    int cnt[26] = {}, key = 0, res = 0;
+    for (char ch : word)
+        ++cnt[ch - 'a'];
+    sort(begin(cnt), end(cnt), greater<>());
+    for (int c : cnt)
+        res += (1 + key++ / 8) * c;
+    return res;
+
+}
 };
 
 int main(){
