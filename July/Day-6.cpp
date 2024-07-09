f033d8efe275d0545103508ad2463ed6a8e44f87
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-09 14:36:23 +0000

    Problem-4 done

--- July/Day-6.cpp
+++ July/Day-6.cpp
@@ -63,6 +63,28 @@ public:
         }
         return sum;
     }
+    string frequencySort(string s) {
+        auto cmp=[](const pair<char,int>& a,const pair<char,int>& b){
+            return a.second<b.second;
+        };
+        unordered_map<char,int> mp;
+        for(auto a:s){
+            mp[a]++;
+        }
+        priority_queue<pair<char,int>,vector<pair<char,int>>,decltype(cmp)> pq(cmp);
+        for(const auto& entry:mp){
+            pq.push(make_pair(entry.first,entry.second));
+        }
+        string result = "";
+        while(!pq.empty()){
+            pair<char,int> p = pq.top();
+            pq.pop();
+            result.append(p.second,p.first);
+        }
+        return result;
+
+       
+    }
 };
 
 int main(){
