a9068ccb4d2cf978c29972383980cf0536630660
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-09 14:21:16 +0000

    Problem-1 done

--- July/Day-5.cpp
+++ July/Day-5.cpp
@@ -18,6 +18,7 @@ public:
             }
         }
         return c+1;
+        
     }
     
 };
---
+++ July/Day-6.cpp
@@ -0,0 +1,33 @@
+// Author - Chekuri Viroopaksh
+// Day 23 - July 9, 2024
+// Sorting Leetcode problems
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), cmp);
+        vector<vector<int>> res(people.size(), vector<int>());
+        for(int i = 0; i < people.size(); i++){
+            int cnt, j;
+            for(cnt = 0, j = 0; j < res.size(); j++){ //empty count start from 0
+                if(res[j].empty()){
+                    if(cnt==people[i][1]) break;
+                    cnt++;
+                }
+            }
+            res[j]=people[i];
+        }
+        return res;
+    }
+    static bool cmp(vector<int>& p1, vector<int>& p2){
+        if(p1[0]!=p2[0]) return p1[0]<p2[0];
+        else return p1[1]>p2[1];
+    }
+};
+
+int main(){
+    return 0;
+}
