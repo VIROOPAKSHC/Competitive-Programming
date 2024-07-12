ed6be69ca8d50f7aa95bfc2760a8ec6d3d86a2a2
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-12 14:37:32 +0000

    Problem-1 done

---
+++ July/Day-9.cpp
@@ -0,0 +1,42 @@
+// Author - Chekuri Viroopaksh
+// Day 26 - July 12, 2024
+// Leetcode
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int total = 0;
+        string high = x>y?"ab":"ba";
+        string low = x>y?"ba":"ab";
+        string final = removesubstring(s,high);
+        total += max(x,y) * (int(s.size()-final.size())/2);
+        string final2 = removesubstring(final,low);
+        total += min(x,y) * (int(final.size()-final2.size())/2);
+        return total;
+    }
+    string removesubstring(string s, string a){
+        stack<char> st;
+        for(auto i:s){
+            if(i==a[1] && !st.empty() && st.top()==a[0]){
+                    st.pop();
+            }
+            else{
+                st.push(i);
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+int main(){
+    return 0;
+}
