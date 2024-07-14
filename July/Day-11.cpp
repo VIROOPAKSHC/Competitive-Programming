2244946e7fd684cd9767582a7feaf784343deaa2
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-14 17:02:10 +0000

    Problem-1 done

---
+++ July/Day-11.cpp
@@ -0,0 +1,36 @@
+// Author - Chekuri Viroopaksh
+// Day 28 - July 24,2024
+// Leetcode Hashing
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mp1;
+        map<int,int> mp2;
+        for(int i:nums1){
+            mp1[i]++;
+        }
+        for(int i:nums2){
+            mp2[i]++;
+        }
+        int c1=0,c2=0;
+        for(int i:nums1){
+            if(mp2[i]){
+                c1++;
+            }
+        }
+        for(int i:nums2){
+            if(mp1[i]){
+                c2++;
+            }
+        }
+        return {c1,c2};
+    }
+};
+
+int main(){
+    return 0;
+}
