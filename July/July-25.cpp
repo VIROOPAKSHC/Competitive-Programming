d622db56ce76f18da07f8c990d52e7be0805ca4a
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-07-31 15:25:03 +0000

    Problem-1 done

--- July/July-25.cpp
+++ July/July-25.cpp
@@ -5,6 +5,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
 int main(){
     
     return 0;
---
+++ July/July-31.cpp
@@ -0,0 +1,40 @@
+// Author - Chekuri Viroopaksh
+// Day 30 - July 31, 2024
+// Leetcode Medium Problems
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>> ans;
+        map<int,int> mp;
+        set<int> s;
+        for(int num:nums){
+            mp[num]++;
+            s.insert(num);
+        }
+        while(true){
+            vector<int> vect;
+            bool f=false;
+            for(auto a:s){
+                if(mp[a]){vect.push_back(a);mp[a]--;}
+                
+                if(mp[a]!=0){
+                    f=true;
+                }
+            }
+            ans.push_back(vect);
+            if(f==false){
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
