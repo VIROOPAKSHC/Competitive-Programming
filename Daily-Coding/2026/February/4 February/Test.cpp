dac6f0123145c89bba524570a93cfda9132d9397
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2026-02-04 17:42:33 +0000

    started github codespaces

---
+++ Daily-Coding/2026/February/4 February/Test.cpp
@@ -0,0 +1,16 @@
+/*
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    priority_queue<int,vector<int>,greater<int>> pq;
+    pq.push(4);
+    pq.push(10);
+    pq.push(-1);
+    cout<<pq.top()<<" ";
+    pq.pop();
+    cout<<pq.top()<<" ";
+    pq.pop();
+    cout<<pq.top()<<endl;
+}
